ea888560567ab6b087d0bd2e526ad51873440b7b
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-02 23:01:29 +0530

    boy idle animation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var boy = document.getElementById("boy");
+var boyIdleImageNumber = 0;
+
+function boyIdleAnimation() {
+    boyIdleImageNumber = boyIdleImageNumber + 1;
+    boy.src = "./boy/Idle (" + boyIdleImageNumber + ").png";
+
+}
