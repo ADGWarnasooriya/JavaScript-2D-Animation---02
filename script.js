f8eafa10ed55b141d8d388fbe3264e8fd394efdb
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-12-06 00:07:05 +0530

    to stop run when jumping

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function keyListner(event) {
         if (boyJumpAnimationStatus == false) {
             boyJumpAnimationStart();
             boyJumpAnimationStatus = true;
-
+            boyRunAnimationStatus = true;
         }
 
     }
@@ -79,8 +79,6 @@ function boyJumpAnimation() {
         boyJumpAnimationStatus = false;
         boyRunAnimationStart();
         var boyRunAnimationStatus = true;
-    } else {
-        boyRunAnimationStatus = true;
     }
 
     boy.src = "./boy/Jump (" + boyJumpImageNumber + ").png";
